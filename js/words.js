9bcd62a607ca28829a5e476780dab89ea095ef26
Ivan-rom <rom_1van@icloud.com>
2024-01-15 19:17:21 +0600

    feat: add words.js file

---
+++ js/words.js
@@ -0,0 +1,14 @@
+export const words = [
+  {
+    word: "pasta",
+    hint: "Unleavened dough, made with wheat or other flour, water, and sometimes eggs, that is molded into any of a variety of shapes and boiled.",
+  },
+  {
+    word: "pizza",
+    hint: "A baked pie of Italian origin consisting of a shallow breadlike crust covered with toppings such as seasoned tomato sauce, cheese, or sausage.",
+  },
+  {
+    word: "dumpling",
+    hint: "A piece of dough, sometimes filled, that is cooked in liquid such as water or soup.",
+  },
+];
