0a75f78257a0544cc5dc13152a8df51fe7e53c3e
Ivan-rom <rom_1van@icloud.com>
2024-01-15 22:09:44 +0600

    feat: add more words for guessing

--- js/words.js
+++ js/words.js
@@ -11,4 +11,32 @@ export const words = [
     word: "dumpling",
     hint: "A piece of dough, sometimes filled, that is cooked in liquid such as water or soup.",
   },
+  {
+    word: "burger",
+    hint: "A sandwich consisting of a bun, a cooked beef patty, and often other ingredients such as cheese, onion slices, lettuce, or condiments. Often used in combination.",
+  },
+  {
+    word: "ramen",
+    hint: "A Japanese dish of noodles in broth, often garnished with small pieces of meat and vegetables.",
+  },
+  {
+    word: "taco",
+    hint: "A corn tortilla folded around a filling such as ground meat or cheese.",
+  },
+  {
+    word: "macaron",
+    hint: "A sweet pastry traditionally made in France.",
+  },
+  {
+    word: "potsticker",
+    hint: "A kind of pan-fried dumpling in East Asian cuisines.",
+  },
+  {
+    word: "fries",
+    hint: "strips of potato fried in deep fat",
+  },
+  {
+    word: "oolong",
+    hint: "A dark Chinese tea that has been partially fermented before drying.",
+  },
 ];
