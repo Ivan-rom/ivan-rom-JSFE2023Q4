53d828f1e71eb798d5dae4dc825f191e400bb3b0
Ivan-rom <rom_1van@icloud.com>
2024-03-11 23:17:54 +0600

    fix: remove part of RSS-PZ-02

--- rss-puzzle/src/components/LoginForm/LoginForm.ts
+++ rss-puzzle/src/components/LoginForm/LoginForm.ts
@@ -10,35 +10,11 @@ const fields: {
         message: string;
     }[];
 } = {
-    name: [
-        {
-            type: 'name-length',
-            test: /^.{3,}$/,
-            message: 'Name must be at least 3 characters long',
-        },
-    ],
-    surname: [
-        {
-            type: 'surname-length',
-            test: /^.{4,}$/,
-            message: 'Surname must be at least 4 characters long',
-        },
-    ],
     errors: [
         {
             type: 'required',
             test: /^.+$/,
             message: 'This field must be filled',
-        },
-        {
-            type: 'symbols',
-            test: /^[a-zA-Z\-]+$/,
-            message: 'Use only English alphabet letters and the hyphen (" - ") symbol',
-        },
-        {
-            type: 'first-letter',
-            test: /^[A-Z].*$/,
-            message: 'The first letter must be in uppercase',
         },
     ],
 };
@@ -69,27 +45,7 @@ export class LoginForm extends BaseComponent<HTMLFormElement> {
             classList: ['label', 'label-required'],
         });
 
-        const inputHandler = (event: Event) => {
-            ul.clear();
-            this.hasErrors = false;
-            this.submitButton.setDisabled(this.hasErrors);
-
-            const target = event.target as HTMLInputElement;
-            const errors = [...fields[target.name], ...fields.errors];
-
-            errors.forEach((error) => {
-                if (!error.test.test(target.value)) {
-                    const errorItem = new BaseComponent({
-                        tagName: 'li',
-                        classList: ['error-item'],
-                        text: error.message,
-                    });
-                    this.hasErrors = true;
-                    this.submitButton.setDisabled(this.hasErrors);
-                    ul.append([errorItem.getComponent()]);
-                }
-            });
-        };
+        const inputHandler = (event: Event) => {};
 
         const input = new Input({ name: name, event: { type: 'input', callback: inputHandler }, required: true });
         this.checkValues(input.getComponent().value);
