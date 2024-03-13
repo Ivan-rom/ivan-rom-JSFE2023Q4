80e9d24964985ae0da7d7512d0aadad8572d3290
Ivan-rom <rom_1van@icloud.com>
2024-03-13 16:40:05 +0600

    feat: implement API

--- rss-puzzle/src/types.ts
+++ rss-puzzle/src/types.ts
@@ -2,3 +2,29 @@ export type ComponentEvent = {
     type: string;
     callback: (() => void) | ((event: Event) => void);
 };
+
+export type Word = {
+    id: number;
+    word: string;
+    wordTranslate: string;
+    audioExample: string;
+    textExample: string;
+    textExampleTranslate: string;
+};
+
+export type Round = {
+    levelData: {
+        id: string;
+        name: string;
+        imageSrc: string;
+        cutSrc: string;
+        author: string;
+        year: string;
+    };
+    words: Word[];
+};
+
+export type GameData = {
+    rounds: Round[];
+    roundsCount: number;
+};
