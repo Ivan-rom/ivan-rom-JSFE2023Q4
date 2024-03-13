e12817202fd602b7e0744e351476813eb666b307
Ivan-rom <rom_1van@icloud.com>
2024-03-13 09:11:22 +0600

    feat: implement styles with appearance for home page

--- rss-puzzle/src/pages/HomePage/HomePage.ts
+++ rss-puzzle/src/pages/HomePage/HomePage.ts
@@ -1,5 +1,9 @@
+import { BaseComponent } from '../../BaseComponent';
+import { Button } from '../../components/Button/Button';
 import { Header } from '../../components/Header/Header';
 import { Page } from '../Page';
+
+import './homePage.css';
 
 export class HomePage extends Page {
     user: { name: string; surname: string } | null;
@@ -11,7 +15,29 @@ export class HomePage extends Page {
 
         const header = new Header();
 
-        this.append([header]);
+        this.append([header, this.createContent()]);
+    }
+
+    createContent(): BaseComponent {
+        const h1 = new BaseComponent({ tag: 'h1', textContent: 'RSS Puzzle Game', className: 'heading' });
+        const text = [
+            new BaseComponent({
+                tag: 'p',
+                className: 'description',
+                textContent: 'Click on words, collect phrases.',
+            }),
+            new BaseComponent({
+                tag: 'p',
+                className: 'description',
+                textContent: 'Words can drag and drop. Select tooltip in the menu.',
+            }),
+        ];
+
+        const clickHandler = (event: Event) => {};
+
+        const button = new Button('Start', clickHandler, 'start-button');
+
+        return new BaseComponent({ className: 'home-content' }, [h1, ...text, button]);
     }
 
     render() {
