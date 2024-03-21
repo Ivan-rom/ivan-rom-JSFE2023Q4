d87e80b8c7b4275f0fc1b206183b0d8871a9665a
Ivan-rom <94216613+Ivan-rom@users.noreply.github.com>
2024-03-21 15:33:00 +0300

    Feat: RSS PZ 31 Implement Sentences Separation (#79)
    * feat: implement sentences separation to two lists (, )
    
    * feat: add styles for lists of sentences

--- rss-puzzle/src/pages/Statistic/Statistic.ts
+++ rss-puzzle/src/pages/Statistic/Statistic.ts
@@ -1,6 +1,6 @@
 import { BaseComponent } from '../../BaseComponent';
 import Button from '../../components/Button/Button';
-import { StatisticType, User } from '../../types';
+import { StatisticType, User, Word } from '../../types';
 import Page from '../Page';
 
 import './statistic.css';
@@ -15,17 +15,12 @@ export default class Statistic extends Page {
 
     render() {
         super.render();
+        this.component.innerHTML = '';
         this.data = JSON.parse(localStorage.getItem('statistic')!);
         const content = new BaseComponent({ className: 'statistic-content' });
-        const ul = new BaseComponent({ tag: 'ul', className: 'words' });
-        this.data.words.forEach((word) => {
-            const wordComponent = new BaseComponent({
-                tag: 'li',
-                className: 'sentence',
-                textContent: word.textExample,
-            });
-            ul.append([wordComponent]);
-        });
+        const knownWords = this.data.words.filter((word) => !word.isSkipped);
+        const unknownWords = this.data.words.filter((word) => word.isSkipped);
+
         const button = new Button(
             'Continue',
             () => {
@@ -33,7 +28,25 @@ export default class Statistic extends Page {
             },
             'continue'
         );
-        content.append([ul, button]);
+        content.append([this.createList(knownWords), this.createList(unknownWords, true), button]);
         this.append([content]);
+    }
+
+    createList(words: (Word & { isSkipped: boolean })[], isSkipped: boolean = false): BaseComponent {
+        const title = new BaseComponent({
+            className: 'list-title',
+            textContent: isSkipped ? "I don't know" : 'I know',
+        });
+        const ul = new BaseComponent({ tag: 'ul', className: 'words' });
+        words.forEach((word) => {
+            const wordComponent = new BaseComponent({
+                tag: 'li',
+                className: 'sentence',
+                textContent: word.textExample,
+            });
+            ul.append([wordComponent]);
+        });
+        const wrapper = new BaseComponent({ className: 'list-wrapper' }, [title, ul]);
+        return wrapper;
     }
 }
