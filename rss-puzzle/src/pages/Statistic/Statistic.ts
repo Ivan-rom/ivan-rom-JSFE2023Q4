93853f15e94c9dfeecb5a66d0cccaaa115050e96
Ivan-rom <94216613+Ivan-rom@users.noreply.github.com>
2024-03-21 14:53:15 +0300

    Feat: RSS PZ 29 Implement Result Button (#77)
    * feat: implement result button
    
    * feat: implement statistic page
    
    * feat: implement basic statistic page styles

---
+++ rss-puzzle/src/pages/Statistic/Statistic.ts
@@ -0,0 +1,32 @@
+import { BaseComponent } from '../../BaseComponent';
+import { StatisticType } from '../../types';
+import Page from '../Page';
+
+import './statistic.css';
+
+export default class Statistic extends Page {
+    data: StatisticType;
+
+    constructor() {
+        super({ className: 'statistic' });
+        this.data = JSON.parse(localStorage.getItem('statistic')!);
+
+        this.createContent();
+    }
+
+    createContent() {
+        this.data = JSON.parse(localStorage.getItem('statistic')!);
+        const content = new BaseComponent({ className: 'statistic-content' });
+        const ul = new BaseComponent({ tag: 'ul', className: 'words' });
+        this.data.words.forEach((word) => {
+            const wordComponent = new BaseComponent({
+                tag: 'li',
+                className: 'sentence',
+                textContent: word.textExample,
+            });
+            ul.append([wordComponent]);
+        });
+        content.append([ul]);
+        this.append([content]);
+    }
+}
