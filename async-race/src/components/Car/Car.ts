65ec7eb2dd6a391e719837d924c432f5ae5e5e0d
Ivan-rom <rom_1van@icloud.com>
2024-03-26 11:28:18 +0600

    feat: implement froms for creating and updating cars, implement removing car

--- async-race/src/components/Car/Car.ts
+++ async-race/src/components/Car/Car.ts
@@ -11,22 +11,35 @@ export default class Car extends BaseComponent {
 
   id: number;
 
-  constructor({ name, color, id }: CarType) {
-    super({ className: "car" });
+  car: BaseComponent;
+
+  updateCallback: () => void;
+
+  constructor({ name, color, id }: CarType, updateCallback: () => void) {
+    super({ className: "car-wrapper" });
     this.name = name;
     this.color = color;
     this.id = id;
 
-    this.component.style.backgroundColor = this.color;
-    this.component.id = `${this.id}`;
-    this.component.title = this.name;
+    this.updateCallback = updateCallback;
 
+    this.car = this.getCar();
     this.createButtons();
+    this.append([this.car]);
+  }
+
+  getCar(): BaseComponent {
+    const car = new BaseComponent({ className: "car" });
+    car.component.style.backgroundColor = this.color;
+    car.component.id = `${this.id}`;
+    car.component.title = this.name;
+    return car;
   }
 
   createButtons() {
     const deleteHandler = () => {
       api.deleteCar(this.id);
+      this.updateCallback();
     };
     const deleteButton = new BaseComponent<HTMLButtonElement>({
       tag: "button",
@@ -35,6 +48,19 @@ export default class Car extends BaseComponent {
       onclick: deleteHandler.bind(this),
     });
 
-    this.append([deleteButton]);
+    const changeHandler = () => {
+      const form = document.forms[1];
+      (form.elements[0] as HTMLInputElement).value = this.name;
+      (form.elements[1] as HTMLInputElement).value = this.color;
+      (form.elements[2] as HTMLInputElement).value = `${this.id}`;
+    };
+    const changeButton = new BaseComponent<HTMLButtonElement>({
+      tag: "button",
+      className: "button change-button",
+      textContent: "Change car",
+      onclick: changeHandler.bind(this),
+    });
+
+    this.append([deleteButton, changeButton]);
   }
 }
--- async-race/src/components/CarForm/CarForm.ts
+++ async-race/src/components/CarForm/CarForm.ts
@@ -3,45 +3,23 @@ import BaseComponent from "../BaseComponent";
 const url = "http://127.0.0.1:3000";
 
 export default class CarForm extends BaseComponent {
-  newColorInput: BaseComponent<HTMLInputElement>;
+  newForm: BaseComponent<HTMLFormElement>;
 
-  newNameInput: BaseComponent<HTMLInputElement>;
-
-  newButton: BaseComponent<HTMLButtonElement>;
+  changeForm: BaseComponent<HTMLFormElement>;
 
   updateCallback: () => void;
 
   constructor(updateCallback: () => void) {
     super({ className: "form" });
-
     this.updateCallback = updateCallback;
-
-    this.newNameInput = new BaseComponent<HTMLInputElement>({
-      tag: "input",
-      name: "newName",
-      type: "text",
-    });
-    this.newColorInput = new BaseComponent<HTMLInputElement>({
-      tag: "input",
-      name: "newColor",
-      type: "color",
-    });
-    this.newButton = new BaseComponent<HTMLButtonElement>({
-      tag: "button",
-      textContent: "create",
-      onclick: this.newHandler.bind(this),
-    });
-
-    this.append([this.newNameInput, this.newColorInput, this.newButton]);
+    this.newForm = this.createNewForm();
+    this.changeForm = this.createChangeForm();
+    this.append([this.newForm, this.changeForm]);
   }
 
-  newHandler() {
-    const body = {
-      name: this.newNameInput.component.value,
-      color: this.newColorInput.component.value,
-    };
-    console.log(JSON.stringify(body));
-
+  newHandler(e: SubmitEvent) {
+    e.preventDefault();
+    const body = Object.fromEntries(new FormData(e.target as HTMLFormElement));
     fetch(`${url}/garage`, {
       method: "POST",
       headers: {
@@ -51,5 +29,84 @@ export default class CarForm extends BaseComponent {
     })
       .then((response) => response.json())
       .then(() => this.updateCallback());
+  }
+
+  changeHandler(e: SubmitEvent) {
+    e.preventDefault();
+    const body = Object.fromEntries(new FormData(e.target as HTMLFormElement));
+    console.log(body);
+
+    fetch(`${url}/garage/${body.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then(() => this.updateCallback());
+  }
+
+  createNewForm(): BaseComponent<HTMLFormElement> {
+    const nameInput = new BaseComponent<HTMLInputElement>({
+      tag: "input",
+      name: "name",
+      type: "text",
+    });
+    const colorInput = new BaseComponent<HTMLInputElement>({
+      tag: "input",
+      name: "color",
+      type: "color",
+    });
+    const button = new BaseComponent<HTMLButtonElement>({
+      tag: "button",
+      textContent: "create",
+    });
+
+    const form = new BaseComponent<HTMLFormElement>(
+      {
+        tag: "form",
+        className: "new-form",
+        name: "new",
+      },
+      [nameInput, colorInput, button],
+    );
+    form.component.onsubmit = this.newHandler.bind(this);
+
+    return form;
+  }
+
+  createChangeForm(): BaseComponent<HTMLFormElement> {
+    const nameInput = new BaseComponent<HTMLInputElement>({
+      tag: "input",
+      name: "name",
+      type: "text",
+    });
+    const colorInput = new BaseComponent<HTMLInputElement>({
+      tag: "input",
+      name: "color",
+      type: "color",
+    });
+    const button = new BaseComponent<HTMLButtonElement>({
+      tag: "button",
+      textContent: "change",
+    });
+    const id = new BaseComponent<HTMLInputElement>({
+      tag: "input",
+      name: "id",
+      type: "hidden",
+    });
+
+    const form = new BaseComponent<HTMLFormElement>(
+      {
+        tag: "form",
+        className: "update-form",
+        name: "update",
+      },
+      [nameInput, colorInput, id, button],
+    );
+    form.component.onsubmit = this.changeHandler.bind(this);
+
+    return form;
   }
 }
