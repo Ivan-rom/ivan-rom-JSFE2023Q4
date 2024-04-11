4d42d01460cd4a6bdbc23a3b3ca80e2ddbf7a120
Ivan-rom <rom_1van@icloud.com>
2024-04-11 13:13:00 +0600

    init: start fun-chat task and prepare eviroment

---
+++ fun-chat/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+        include: path.resolve(__dirname, "src"),
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        include: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["", ".ts", ".tsx", ".js"],
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
